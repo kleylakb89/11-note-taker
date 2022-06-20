7cf542477bd1e7dc9fcbcffa6364fedda3a72362
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-20 17:40:32 -0400

    started basic server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.listen(PORT, () => console.log(`Page is at http://localhost:${PORT}`));
