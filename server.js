3e166be4f6f27d37fc177c298686254e9d96076a
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-22 12:16:33 -0400

    preparing for heroku deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const uniqid = require('uniqid');
 const fs = require('fs');
 const db = require('./db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
