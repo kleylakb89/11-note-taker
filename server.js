94dce8ca06bc2d662521b10e6e3532d081853cb2
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-22 15:17:51 -0400

    added comments

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
+// requiring packages for use in server
 const express = require('express');
 const path = require('path');
+// uniqid will create a randomized id for every note
 const uniqid = require('uniqid');
 const fs = require('fs');
 
+// setting port so it will either run on Heroku host or locally
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// middleware for use app.post and accessing public folder
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// setting the landing page and /notes pages to their respective HTMLs
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+// will read the json file and return a response of that data array
 app.get('/api/notes', (req, res) => {
     const notesData = fs.readFileSync('./db/db.json', 'utf8');
     const notesArr = notesData.length ? JSON.parse(notesData) : [];
     return res.json(notesArr);
 });
 
+// bad paths will lead to the index page
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
+// allows user to post new notes - first reads the json file and creates a data array, then pushes the new note with a unique id into that array, stringifies it, and then writes to file
 app.post('/api/notes', (req, res) => {
     if (req.body) {
         const notesData = fs.readFileSync('./db/db.json', 'utf8');
@@ -32,6 +40,7 @@ app.post('/api/notes', (req, res) => {
     } else return res.json('Note must contain data.');
 });
 
+// delete request for specific notes. Reads the file and makes a data array, filters the array based on the searched for id value, stringfies the new notes without the searched note, and writes to file so the searched note is removed from the json.
 app.delete('/api/notes/:id', (req, res) => {
     const notesData = fs.readFileSync('./db/db.json', 'utf8');
     const notesArr = notesData.length ? JSON.parse(notesData) : [];
@@ -42,4 +51,5 @@ app.delete('/api/notes/:id', (req, res) => {
     fs.writeFile('./db/db.json', dbString, err => err ? console.log(err) : res.json('Note deleted!'));
 });
 
+// listening for responses
 app.listen(PORT, () => console.log(`Page is at http://localhost:${PORT}`));
